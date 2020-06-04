54284e54040072fad43c3a6411bf123986c306f7
Ian Gregg <iangregg188@gmail.com>
2020-06-04 14:51:40 -0700

    Debugged the JS for the Triangle tracker & started refactoring the JS elements
    Co-authored-by: Ian Scott <chesnekov@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,43 @@
+const eq1= A + B <= C
+const eq2= 
+const eq3=
+
 $(document).ready(function(){
-  $("form#triangle").submit
-}
+  $("form#triangle").submit(function(event) {
+    event.preventDefault();
+    
+    const num1 = parseInt($(".number1").val());
+    const num2 = parseInt($(".number2").val());
+    const num3 = parseInt($(".number3").val());
+
+    if (num1 + num2 >=num3 || num2 + num3 >= num1 || num3 + num1 >= num2) {
+
+      if (num1 === num2 && num2 === num3 && num3 === num1){
+        $(".equilateral").show();
+        $(".isosceles").hide();
+        $(".scalene").hide();
+        $(".notTriangle").hide();
+      }
+  
+      else if (num1 === num2 || num2 === num3 || num3 === num1){
+        $(".equilateral").hide();
+        $(".isosceles").show();
+        $(".scalene").hide();
+        $(".notTriangle").hide();
+      }
+  
+      else {
+        $(".equilateral").hide();
+        $(".isosceles").hide();
+        $(".scalene").show();
+        $(".notTriangle").hide();
+    }
+  }
+    else {
+      $(".equilateral").hide();
+      $(".isosceles").hide();
+      $(".scalene").hide();
+      $(".notTriangle").show();
+    }
+  });
+});
